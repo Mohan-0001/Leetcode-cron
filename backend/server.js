ccf70ab0eaf85c348438ecb6238a6a3262a4715d
Mohan Gupta <mohan.gupta_cs23@gla.ac.in>
2025-12-30 22:02:25 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -144,4 +144,5 @@ app.get("/api/leaderboard", async (req, res) => {
   res.json(users);
 });
 
-app.listen(5000, () => console.log("🚀 Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
